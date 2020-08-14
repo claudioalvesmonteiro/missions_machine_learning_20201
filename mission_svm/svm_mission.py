8d157639ab903c42312ba3ba89f6c59e4271b226
Cláudio Monteiro <clam@cin.ufpe.br>
2020-08-14 14:49:42 -0300

    up

---
+++ mission_svm/svm_mission.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+#  https://classroom.google.com/u/0/c/MTA1MjE1NDkxNTg2/a/MTQyNzE3MjM1OTM0/details
+
+# https://archive.ics.uci.edu/ml/datasets/glass+identification
+
+
+# import packages
+import pandas as pd 
+
+# import data
+df = pd.read_csv('mission_svm/glass.data',header=None)
+
+
+#=====================================
+# preprocessing
+#====================================
+
+# normalizar
+
+# gerar graficos de frequencia e boxplot
+
+# teste de correlacao
+
+# teste KS entre variavel continua e classes
+
+#===================
+# experimento SVM
+#==================
